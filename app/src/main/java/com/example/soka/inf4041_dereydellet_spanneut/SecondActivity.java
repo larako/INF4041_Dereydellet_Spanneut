53d29511b94b6cd2737e0e87c12439133b61c39e
DE REYDELLET Romain <ETD-P\dereydellet@v-12-13-lx.etd-p.esiea.fr>
2016-11-07 16:12:30 +0100

    Add second activity + 1 service

---
+++ app/src/main/java/com/example/soka/inf4041_dereydellet_spanneut/SecondActivity.java
@@ -0,0 +1,13 @@
+package com.example.soka.inf4041_dereydellet_spanneut;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_activity);
+    }
+}
