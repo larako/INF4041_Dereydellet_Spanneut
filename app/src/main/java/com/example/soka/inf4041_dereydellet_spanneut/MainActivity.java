e96fa3c07ad1bdb5265614d5de3c056f3f7b6239
DE REYDELLET Romain <ETD-P\dereydellet@v-12-13-lx.etd-p.esiea.fr>
2016-11-07 14:50:02 +0100

    All lib + button with onclick listener

--- app/src/main/java/com/example/soka/inf4041_dereydellet_spanneut/MainActivity.java
+++ app/src/main/java/com/example/soka/inf4041_dereydellet_spanneut/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.soka.inf4041_dereydellet_spanneut;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.btn_clickme);
+        button1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getBaseContext(), "Hello" , Toast.LENGTH_SHORT ).show();
+            }
+        });
     }
 }
