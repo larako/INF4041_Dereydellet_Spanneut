d55c6b412f171fbdf3a5463cf7b48cec619c92ec
Romain de Reydellet <romain.dereydellet@gmail.com>
2016-11-07 11:38:30 +0100

    Add android files

---
+++ app/src/main/java/com/example/soka/inf4041_dereydellet_spanneut/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.soka.inf4041_dereydellet_spanneut;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
