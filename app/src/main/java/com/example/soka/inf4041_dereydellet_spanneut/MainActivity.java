b97e7801e8389f2d06b241442b57b95b6f6820f7
cyril martin <cyril martin>
2016-11-14 13:56:19 +0100

    accés page contact

--- app/src/main/java/com/example/soka/inf4041_dereydellet_spanneut/MainActivity.java
+++ app/src/main/java/com/example/soka/inf4041_dereydellet_spanneut/MainActivity.java
@@ -3,9 +3,13 @@ package com.example.soka.inf4041_dereydellet_spanneut;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.content.Context;
+import android.database.Cursor;
 import android.graphics.Color;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v4.app.NotificationCompat;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -14,13 +18,14 @@ import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.ToggleButton;
 
+
+import java.lang.reflect.Method;
 import java.util.Calendar;
 
 public class MainActivity extends Activity {
@@ -33,6 +38,7 @@ public class MainActivity extends Activity {
     private ToggleButton toggleButton;
     private boolean ispush=false;
     private NotificationManager alarmNotificationManager;
+    static final int PICK_CONTACT_REQUEST = 1;  // The request code
 
     public static MainActivity instance() {
         return inst;
@@ -57,24 +63,24 @@ public class MainActivity extends Activity {
         alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
     }
 
-    public void click(View view) {
+    public void clickStart(View view) { //permet de start l'appli
         if (((ToggleButton) view).isChecked()) {
             setToast("activé");
-            Calendar newTime = Calendar.getInstance();
+            Calendar newTime = Calendar.getInstance(); // pour recuperer lheure
             newTime.set(Calendar.HOUR_OF_DAY, time.getCurrentHour());
             newTime.set(Calendar.MINUTE, time.getCurrentMinute());
             ispush=true;
-            createNotification("blocage activé");
-            //toggleButton.setText("Stop");
-            Intent intent = new Intent(MainActivity.this, AlarmReceiver.class);
-            pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
-            alarmManager.set(AlarmManager.RTC, newTime.getTimeInMillis(), pendingIntent);
+             createNotification("blocage activé");
+            Intent intent = new Intent(MainActivity.this, AlarmReceiver.class); //permet d'acceder a la classe alarmReceiver
+            pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0); // permet de decaler le moment d'appeler la fct
+            alarmManager.set(AlarmManager.RTC, newTime.getTimeInMillis(), pendingIntent); //cree l'alarme uand c'est finit
+
         } else {
             cancel();
         }
     }
 
-    public void cancel(){
+    public void cancel(){ // ce qui va annuler l'alarme
         alarmManager.cancel(pendingIntent);
         ispush=false;
         //toggleButton.setText("Start");
@@ -82,9 +88,49 @@ public class MainActivity extends Activity {
         setToast("désactivé");
     }
 
+    public void clickContact(View v){ //permet de selectionner un contact
+        Intent pickContactIntent = new Intent(Intent.ACTION_PICK, Uri.parse("content://contacts"));
+        pickContactIntent.setType(ContactsContract.CommonDataKinds.Phone.CONTENT_TYPE); // Show user only contacts w/ phone numbers
+        startActivityForResult(pickContactIntent, PICK_CONTACT_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){ //des que la personne selectionne des contacts
+        // Check which request it is that we're responding to
+        if (requestCode == PICK_CONTACT_REQUEST) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                // Get the URI that points to the selected contact
+                Uri contactUri = data.getData();
+                // We only need the NUMBER column, because there will be only one row in the result
+                String[] projection = {ContactsContract.CommonDataKinds.Phone.NUMBER};
+
+                // Perform the query on the contact to get the NUMBER column
+                // We don't need a selection or sort order (there's only one result for the given URI)
+                // CAUTION: The query() method should be called from a separate thread to avoid blocking
+                // your app's UI thread. (For simplicity of the sample, this code doesn't do that.)
+                // Consider using CursorLoader to perform the query.
+                Cursor cursor = getContentResolver()
+                        .query(contactUri, projection, null, null, null);
+                cursor.moveToFirst();
+
+                // Retrieve the phone number from the NUMBER column
+                int column = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+                String number = cursor.getString(column);
+
+                setToast("contact ajouté");
+                    //et la on ajoute ce numero à la liste des gens autorisés
+            }
+        }
+    }
+
+
+
+
     public void setToast(String text){
         Toast.makeText(getBaseContext(), text , Toast.LENGTH_SHORT ).show();
     }
+
 
     public void createNotification(String message){
 
