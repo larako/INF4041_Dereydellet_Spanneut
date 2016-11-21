ea6fd6003fd8ccbecd2bbc6ba51a1593547cc5fa
cyril martin <cyril martin>
2016-11-21 20:04:19 +0100

    sms bloqués

--- app/src/main/java/com/example/soka/inf4041_dereydellet_spanneut/SmsReceiver.java
+++ app/src/main/java/com/example/soka/inf4041_dereydellet_spanneut/SmsReceiver.java
@@ -12,31 +12,38 @@ import static java.lang.Thread.sleep;
  */
 
 public  class SmsReceiver extends BroadcastReceiver {
+
+
     @Override
     public void onReceive(Context context, Intent intent) {
         String MSG_TYPE=intent.getAction();
         DatabaseController db;
         db = new DatabaseController(context);
         String app_is_up = db.getSettingByName("block_status");
+        app_is_up=app_is_up.intern();
         if(MSG_TYPE.equals("android.provider.Telephony.SMS_RECEIVED")) {
+            MainActivity inst = MainActivity.instance();
             if (app_is_up=="stop") {
                 System.out.print("ca passe");
+                AudioManager recupAudio = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+                int recup=recupAudio.getRingerMode();
                 AudioManager audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-                AudioManager recupAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-                audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+                audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+
                 try {
                     sleep(2000);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                if (recupAudioManager.getRingerMode() == AudioManager.RINGER_MODE_VIBRATE)
+
+                if (recup == AudioManager.RINGER_MODE_VIBRATE)
                     audioManager.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
 
-                if (recupAudioManager.getRingerMode() == AudioManager.RINGER_MODE_SILENT)
+                if (recup == AudioManager.RINGER_MODE_SILENT)
                     audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
 
-                if (recupAudioManager.getRingerMode() == AudioManager.RINGER_MODE_SILENT)
-                    audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+                if (recup == AudioManager.RINGER_MODE_NORMAL)
+                    audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
             }
         }
 
